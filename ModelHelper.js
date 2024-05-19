d587805d1f268bdc71a588409d5d95af425575b6
Uriel Garcilazo <garcilazo.uriel@gmail.com>
2024-05-19 01:27:56 -0400

    Updates

--- ModelHelper.js
+++ ModelHelper.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 // import { GLTFLoader } from 'three/addons/loaders';
 
-const scenePath = 'scene2.gltf';
+const scenePath = '/scene2.gltf';
 
 
 // FD. LoadGLTFPath
